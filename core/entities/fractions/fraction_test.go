3c9d14d52a2f23206bc3fc0d341a1d922788753d
tolelom <tolelom@naver.com>
2024-08-27 17:50:57 +0900

    add test

--- core/entities/fractions/fraction_test.go
+++ core/entities/fractions/fraction_test.go
@@ -7,9 +7,7 @@ import (
 func TestFraction_Add(t *testing.T) {
 	fraction1 := NewFraction(1, 2)
 	fraction2 := NewFraction(1, 3)
-
 	result := fraction1.Add(fraction2)
-
 	expected := NewFraction(5, 6)
 
 	if result.Numerator.Cmp(expected.Numerator) != 0 || result.Denominator.Cmp(expected.Denominator) != 0 {
@@ -22,6 +20,7 @@ func TestFraction_Sub(t *testing.T) {
 	fraction2 := NewFraction(1, 3)
 	result := fraction1.Sub(fraction2)
 	expected := NewFraction(1, 6)
+
 	if result.Numerator.Cmp(expected.Numerator) != 0 || result.Denominator.Cmp(expected.Denominator) != 0 {
 		t.Fatalf("Sub: expected %v, got %v", expected, result)
 	}
@@ -35,5 +34,27 @@ func TestFraction_LessThan(t *testing.T) {
 
 	if result != expected {
 		t.Fatalf("LessThan: expected %v, got %v", expected, result)
+	}
+}
+
+func TestFraction_GreaterThan(t *testing.T) {
+	fraction1 := NewFraction(1, 2)
+	fraction2 := NewFraction(1, 3)
+	result := fraction1.GreaterThan(fraction2)
+	expected := true
+
+	if result != expected {
+		t.Fatalf("GreaterThan: expected %v, got %v", expected, result)
+	}
+}
+
+func TestFraction_Equals(t *testing.T) {
+	fraction1 := NewFraction(1, 2)
+	fraction2 := NewFraction(1, 3)
+	result := fraction1.Equals(fraction2)
+	expected := false
+
+	if result != expected {
+		t.Fatalf("Equals: expected %v, got %v", expected, result)
 	}
 }
